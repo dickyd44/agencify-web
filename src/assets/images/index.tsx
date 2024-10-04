df3fd821085364ae92fc30721283bb851c4a851e
dickyd44 <dikydrmwn08@gmail.com>
2024-10-04 21:48:11 +0700

    fix navbar

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,12 @@
+import Magento from "./dropdown-expertise/magento.jpg";
+import Wordpress from "./dropdown-expertise/wordpress.jpg";
+import Laravel from "./dropdown-expertise/laravel.jpg";
+import Woo from "./dropdown-expertise/woocommerce.jpg";
+
+import Optimize from "./dopdown-service/optimisation.png";
+import Design from "./dopdown-service/design.png";
+import Development from "./dopdown-service/development.png";
+import Suivi from "./dopdown-service/suivi.png";
+
+export { Magento, Wordpress, Laravel, Woo };
+export { Optimize, Design, Development, Suivi };
--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -1,9 +1,24 @@
 "use client";
 import Image from "next/image";
 import logo from "@/assets/images/agencify-logo.png";
-import { ArrowRightIcon } from "@/assets/icon-dropdown";
+import {
+  ArrowRightIcon,
+  ChevronDownIcon,
+  VerifiedIcon,
+} from "@/assets/icon-dropdown";
+import {
+  Magento,
+  Wordpress,
+  Laravel,
+  Woo,
+  Optimize,
+  Design,
+  Development,
+  Suivi,
+} from "@/assets/images";
 import { useEffect, useState } from "react";
 
+// Data for the navbar and dropdown items
 const LINK_NAVBAR = [
   {
     name: "accueil",
@@ -12,10 +27,63 @@ const LINK_NAVBAR = [
   {
     name: "notre expertise",
     link: "#",
+    dropdown: [
+      {
+        name: "Magento",
+        description: "e CMS incontournable",
+        link: "#",
+        icon: Magento,
+      },
+      {
+        name: "Wordpress",
+        description: "Le CMS le plus connu",
+        link: "#",
+        icon: Wordpress,
+      },
+      {
+        name: "Laravel",
+        description: "Le Framework compétent",
+        link: "#",
+        icon: Laravel,
+      },
+      {
+        name: "WooCommerce",
+        description: "Création de commerce",
+        link: "#",
+        icon: Woo,
+      },
+    ],
   },
   {
     name: "nos service",
     link: "#",
+    dropdown: [
+      {
+        name: "Optimisation",
+        description: "Web",
+        link: "#",
+        icon: Optimize,
+        verified: true,
+      },
+      {
+        name: "Design graphique",
+        description: "Web",
+        link: "#",
+        icon: Design,
+      },
+      {
+        name: "Développement web",
+        description: "Web",
+        link: "#",
+        icon: Development,
+      },
+      {
+        name: "Suivi & support",
+        description: "Web",
+        link: "#",
+        icon: Suivi,
+      },
+    ],
   },
   {
     name: "a propos",
@@ -28,6 +96,8 @@ const LINK_NAVBAR = [
 ];
 
 export default function Navbar() {
+  const [openDropdownIndex, setOpenDropdownIndex] = useState(null);
+  const [linkActive, setLinkActive] = useState<number | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
 
@@ -51,11 +121,19 @@ export default function Navbar() {
     };
   }, [scrollPos]);
 
+  const handleDropdown = (index: any) => {
+    setOpenDropdownIndex(openDropdownIndex === index ? null : index);
+  };
+
+  useEffect(() => {
+    setLinkActive(0);
+  }, []);
+
   return (
     <nav className="bg-white border-b shadow-lg">
       <div className="container">
         <div className="w-full h-24 px-5 md:px-10 md:py-6 flex items-center justify-between">
-          {/* icon */}
+          {/* logo */}
           <a href="#">
             <Image
               src={logo}
@@ -81,13 +159,67 @@ export default function Navbar() {
           {/* menu */}
           <div className="hidden md:flex items-center gap-4 lg:gap-8 text-black text-sm lg:text-base font-medium">
             {LINK_NAVBAR.map((item, index) => (
-              <a
-                key={index}
-                href={item.link}
-                className="capitalize hover:text-pink"
-              >
-                {item.name}
-              </a>
+              <div key={index} className="relative group">
+                <a
+                  href={item.link}
+                  onClick={() => {
+                    handleDropdown(index);
+                    setLinkActive(index);
+                  }}
+                  className={`flex capitalize hover:text-pink cursor-pointer ${
+                    linkActive === index ? "text-pink" : ""
+                  }`}
+                >
+                  {item.name}
+                  {item.dropdown && (
+                    <span
+                      className={`ml-2 transform transition-transform ${
+                        openDropdownIndex === index ? "rotate-180" : ""
+                      }`}
+                    >
+                      <ChevronDownIcon />
+                    </span>
+                  )}
+                </a>
+
+                {/* Dropdown for specific items */}
+                {item.dropdown && openDropdownIndex === index && (
+                  <div className="absolute top-10 left-0 w-[20rem] bg-white border rounded-md shadow-lg p-4">
+                    {item.dropdown.map((dropdownItem, i) => (
+                      <a
+                        href={dropdownItem.link}
+                        key={i}
+                        className="flex items-center gap-3 py-4 hover:bg-gray-100 rounded-md px-2"
+                      >
+                        {/* Add your icon handling here */}
+                        <div>
+                          <Image
+                            src={dropdownItem.icon}
+                            alt={dropdownItem.name}
+                            width={100}
+                            height={100}
+                            className="object-contain w-[40px] h-[40px]"
+                          />
+                        </div>
+                        <div className="flex flex-col">
+                          <p className="flex items-center font-semibold text-sm">
+                            {dropdownItem.name}
+                            {dropdownItem.verified && (
+                              <span className="flex items-center justify-center gap-1.5 ml-3 text-[10px] text-green-600 bg-green-100 rounded-full w-32 h-6">
+                                <VerifiedIcon />
+                                La plus populaire
+                              </span>
+                            )}
+                          </p>
+                          <p className="text-xs text-gray-500">
+                            {dropdownItem.description}
+                          </p>
+                        </div>
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </div>
             ))}
           </div>
 
